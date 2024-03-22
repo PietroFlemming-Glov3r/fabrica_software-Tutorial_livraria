1bbc8912c5464ed40688c7b2f8ece4e4c34fb586
Pietro Flemming <pietroflemming@gmail.com>
2024-03-22 15:01:16 -0300

    commit adicionando itensCompra ao serializers/compra.py

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -1,6 +1,13 @@
 from rest_framework.serializers import ModelSerializer, CharField
 
-from livraria.models import Compra
+from livraria.models import Compra, ItensCompra
+
+
+class ItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ["livro", "quantidade"]
+        depth = 2
 
 class CompraSerializer(ModelSerializer):
     class Meta:
@@ -8,3 +15,4 @@ class CompraSerializer(ModelSerializer):
         usuario = CharField(source="usuario.email", read_only=True)
         status = CharField(source="get_status_display", read_only=True)
         fields = "__all__"
+        itens = ItensCompraSerializer(many=True, read_only=True)
