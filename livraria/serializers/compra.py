3c402d32efd38786d3138475fdaf3c0e5172ae69
Pietro Flemming <pietroflemming@gmail.com>
2024-03-22 14:57:23 -0300

    commit adicionando status ao serializers/compra.py

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -6,4 +6,5 @@ class CompraSerializer(ModelSerializer):
     class Meta:
         model = Compra
         usuario = CharField(source="usuario.email", read_only=True)
+        status = CharField(source="get_status_display", read_only=True)
         fields = "__all__"
