9ff157c392c2bd82a9e67b20d483eec14c646918
Pietro Flemming <pietroflemming@gmail.com>
2024-03-22 15:22:46 -0300

    commit adicionando edicao da compra

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -19,3 +19,33 @@ class CompraSerializer(ModelSerializer):
         status = CharField(source="get_status_display", read_only=True)
         fields = ("id", "usuario", "status", "total", "itens")
         itens = ItensCompraSerializer(many=True, read_only=True)
+    
+    def update(self, instance, validated_data):
+        itens = validated_data.pop("itens")
+        if itens:
+            instance.itens.all().delete()
+            for item in itens:
+                ItensCompra.objects.create(compra=instance, **item)
+        instance.save()
+        return instance
+
+    
+class CriarEditarItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ("livro", "quantidade")
+
+class CriarEditarCompraSerializer(ModelSerializer):
+    itens = CriarEditarItensCompraSerializer(many=True) # Aqui mudou
+
+    class Meta:
+        model = Compra
+        fields = ("usuario", "itens")
+
+    def create(self, validated_data):
+        itens_data = validated_data.pop("itens")
+        compra = Compra.objects.create(**validated_data)
+        for item_data in itens_data:
+            ItensCompra.objects.create(compra=compra, **item_data)
+        compra.save()
+        return compra
