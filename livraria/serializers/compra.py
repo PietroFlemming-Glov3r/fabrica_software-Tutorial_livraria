5dce3890126904cd59db3dbc1cbc49ec71d28311
Pietro Flemming <pietroflemming@gmail.com>
2024-03-22 15:11:54 -0300

    commit delimitando a area de apresentacao da CategoriaSerializers

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -17,5 +17,5 @@ class CompraSerializer(ModelSerializer):
         model = Compra
         usuario = CharField(source="usuario.email", read_only=True)
         status = CharField(source="get_status_display", read_only=True)
-        fields = "__all__"
+        fields = ("id", "usuario", "status", "total", "itens")
         itens = ItensCompraSerializer(many=True, read_only=True)
